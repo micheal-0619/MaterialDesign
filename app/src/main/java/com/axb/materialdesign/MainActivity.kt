674bdc9d18c04de2a321248f01d934414a516e0e
Michael_0619 <any_leon@163.com>
2023-08-12 09:55:37 +0800

    [Toolbar]Toolbar

--- app/src/main/java/com/axb/materialdesign/MainActivity.kt
+++ app/src/main/java/com/axb/materialdesign/MainActivity.kt
@@ -2,10 +2,33 @@ package com.axb.materialdesign
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import com.axb.materialdesign.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.backup->Toast.makeText(this,"You clicked Backup",Toast.LENGTH_LONG).show()
+            R.id.delete->Toast.makeText(this,"You clicked Delete",Toast.LENGTH_LONG).show()
+            R.id.settings->Toast.makeText(this,"You clicked Settings",Toast.LENGTH_LONG).show()
+        }
+        return true
     }
 }
