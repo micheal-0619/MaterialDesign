acce9ca94630ba276f50700768a51b73df70f060
Michael_0619 <any_leon@163.com>
2023-08-12 10:41:33 +0800

    [DrawerLayout]use DrawerLayout and display home button

--- app/src/main/java/com/axb/materialdesign/MainActivity.kt
+++ app/src/main/java/com/axb/materialdesign/MainActivity.kt
@@ -1,21 +1,28 @@
 package com.axb.materialdesign
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
+import androidx.core.view.GravityCompat
 import com.axb.materialdesign.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    @SuppressLint("RestrictedApi")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setSupportActionBar(binding.toolbar)
+        supportActionBar?.let {
+            it.setDisplayHomeAsUpEnabled(true)
+            it.setHomeAsUpIndicator(R.drawable.ic_menu)
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -24,10 +31,11 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when(item.itemId){
-            R.id.backup->Toast.makeText(this,"You clicked Backup",Toast.LENGTH_LONG).show()
-            R.id.delete->Toast.makeText(this,"You clicked Delete",Toast.LENGTH_LONG).show()
-            R.id.settings->Toast.makeText(this,"You clicked Settings",Toast.LENGTH_LONG).show()
+        when (item.itemId) {
+            R.id.backup -> Toast.makeText(this, "You clicked Backup", Toast.LENGTH_LONG).show()
+            R.id.delete -> Toast.makeText(this, "You clicked Delete", Toast.LENGTH_LONG).show()
+            R.id.settings -> Toast.makeText(this, "You clicked Settings", Toast.LENGTH_LONG).show()
+            android.R.id.home-> binding.drawerLayout.openDrawer(GravityCompat.START)
         }
         return true
     }
